35cada94d17a505a0fd1a767ed5a7713d67158c2
nicejs-is-cool <nicejsalt@gmail.com>
2026-03-19 11:01:21 -0300

    add portrait resource

---
+++ src/scripts/resources/portrait.gd
@@ -0,0 +1,14 @@
+extends Resource
+class_name PMDCollab_Portrait
+
+## Name of the emotion/portrait as seen on the PMDCollab Sprite Repository.
+@export var emotion: String = "Normal"
+## The image associated with that emotion.
+@export var texture: Texture2D
+
+func _init(p_emotion: String = "Normal", p_texture: Texture2D = Texture2D.new()) -> void:
+	emotion = p_emotion
+	texture = p_texture
+
+func new() -> void:
+	pass
