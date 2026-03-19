7b76f0b8587193bbe71b5d2601c5126d4b4cfa0a
nicejs-is-cool <nicejsalt@gmail.com>
2026-03-19 11:12:57 -0300

    add emotion enum

--- src/scripts/resources/portrait.gd
+++ src/scripts/resources/portrait.gd
@@ -1,14 +1,40 @@
 extends Resource
 class_name PMDC_Portrait
 
+enum Emotion {
+	Angry,
+	Crying,
+	Determined,
+	Dizzy,
+	Happy,
+	Inspired,
+	Joyous,
+	Normal,
+	Pain,
+	Sad,
+	Shouting,
+	Sigh,
+	Stunned,
+	Surprised,
+	TearyEyed,
+	Worried,
+	Special0,
+	Special1,
+	Special2,
+	Special3,
+	Special4
+}
 ## Name of the emotion/portrait as seen on the PMDCollab Sprite Repository.
-@export var emotion: String = "Normal"
+@export var emotion: Emotion = Emotion.Normal
 ## The image associated with that emotion.
 @export var texture: Texture2D
 
-func _init(p_emotion: String = "Normal", p_texture: Texture2D = Texture2D.new()) -> void:
+func _init(p_emotion: Emotion = Emotion.Normal, p_texture: Texture2D = Texture2D.new()) -> void:
 	emotion = p_emotion
 	texture = p_texture
 
 func new() -> void:
 	pass
+
+static func str_to_emotion(name: String) -> Emotion:
+	return Emotion[name]
